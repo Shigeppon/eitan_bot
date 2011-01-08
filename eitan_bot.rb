301ce68c7821c059cb90a5db7b3770057f5b18d6
Shigeppon <shigeo.sakamoto@gmail.com>
2011-01-09 00:04:38 +0900

    initial commit

---
+++ eitan_bot.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'twitter_oauth'
+
+t = TwitterOAuth::Client.new({
+  :consumer_key=>"AnBK0IZHO8Fwjyb18IslA",
+  :consumer_secret=>"5mwVXuKMBaDuLX6AI98BDhtd35Eqgv3CUTe4tSQhoc4",
+  :token=>"235550669-TFN1sfj8EQKoqQOoZbts8pRHZwoWtjrjbAid9z6C",
+  :secret=>"meu8R8jKJlOiVBINTBciSpHJmY0svllHcwGx4b0n7g"
+})
+
+words = ["smash","sneeze","struggle","stupid","substance"]
+
+t.update words[rand(words.size)]
+
+#p Marshal.dump(t)
