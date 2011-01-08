7299494f5d2548685cf4a9fb5c506787f84d301f
Shigeo Sakamot <shigeo.sakamoto@gmail.com>
2011-01-09 02:04:34 +0900

    delete key and secret

--- eitan_bot.rb
+++ eitan_bot.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'twitter_oauth'
 
 t = TwitterOAuth::Client.new({
-  :consumer_key=>"AnBK0IZHO8Fwjyb18IslA",
-  :consumer_secret=>"5mwVXuKMBaDuLX6AI98BDhtd35Eqgv3CUTe4tSQhoc4",
-  :token=>"235550669-TFN1sfj8EQKoqQOoZbts8pRHZwoWtjrjbAid9z6C",
-  :secret=>"meu8R8jKJlOiVBINTBciSpHJmY0svllHcwGx4b0n7g"
+  :consumer_key=>"",
+  :consumer_secret=>"",
+  :token=>"",
+  :secret=>""
 })
 
 words = ["smash","sneeze","struggle","stupid","substance"]
