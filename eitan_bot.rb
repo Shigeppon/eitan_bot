39d628cc5d18e242f87a6b1c40558f61d7d3c8d5
Shigeo Sakamot <shigeo.sakamoto@gmail.com>
2011-01-09 02:26:29 +0900

    use secret.xml

--- eitan_bot.rb
+++ eitan_bot.rb
@@ -1,11 +1,21 @@
 require 'rubygems'
 require 'twitter_oauth'
+require 'rexml/document'
+
+include REXML
+
+doc = Document.new File.new("secret.xml")
+
+consumer_key = doc.elements['/root/consumer_key'].text
+consumer_secret = doc.elements['/root/consumer_secret'].text
+token = doc.elements['/root/token'].text
+secret = doc.elements['/root/secret'].text
 
 t = TwitterOAuth::Client.new({
-  :consumer_key=>"",
-  :consumer_secret=>"",
-  :token=>"",
-  :secret=>""
+  :consumer_key=>consumer_key,
+  :consumer_secret=>consumer_secret,
+  :token=>token,
+  :secret=>secret
 })
 
 words = ["smash","sneeze","struggle","stupid","substance"]
